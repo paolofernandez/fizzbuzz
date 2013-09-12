0aef1499ccfe61bff9a6092c3bb4d4cb19590f67
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2013-09-12 01:33:39 -0400

    imprimir una serie de numeros con limite superior en fizzbuzz sin cero

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,14 +5,17 @@ public class FizzBuzz {
 	public String imprimir(int num)
 	{
 		String mensaje = "";
-		if (!(num%3==0) && !(num%5==0))
-			mensaje += num;
-		else
+		if (num>0)
 		{
-			if (num%3==0)
-				mensaje += "fizz";
-			if (num%5==0)
-				mensaje += "buzz";
+			if (!(num%3==0) && !(num%5==0))
+				mensaje += num;
+			else
+			{
+				if (num%3==0)
+					mensaje += "fizz";
+				if (num%5==0)
+					mensaje += "buzz";
+			}
 		}
 		return mensaje;
 	}
